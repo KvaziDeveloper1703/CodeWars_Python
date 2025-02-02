1f026466ad4786063f686ee9b01603e56cda0b90
Viktor <viktor.sbruev.1999@gmail.com>
2025-02-03 00:22:39 +0200

    Added CW_8_20

---
+++ level_8/CW_8_20.py
@@ -0,0 +1,13 @@
+"""
+Create a function that accepts a parameter (a name) and returns the message:
+"Hello, <name> how are you doing today?"
+
+Создайте функцию, которая принимает параметр (имя) и возвращает сообщение:
+"Hello, <name> how are you doing today?"
+"""
+
+def greet(name):
+    return f"Hello, {name} how are you doing today?"
+
+print(greet("John"))
+print(greet("Alice"))
