c308730f1de62ef4b864a29261a382a35661cf50
Viktor <viktor.sbruev.1999@gmail.com>
2025-02-03 00:32:57 +0200

    Added CW_7_11

---
+++ level_7/CW_7_11.py
@@ -0,0 +1,35 @@
+"""
+Check if a string has the same amount of 'x' and 'o'.
++ The function must return a boolean (true or false).
++ It must be case insensitive.
++ The string can contain any characters.
+
+Examples:
+xo("ooxx") → true
+xo("xooxx") → false
+xo("ooxXm") → true
+xo("zpzpzpp") → true
+xo("zzoo") → false
+
+Проверьте, содержит ли строка одинаковое количество символов 'x' и 'o'.
++ Функция должна возвращать булево значение (true или false).
++ Проверка не чувствительна к регистру.
++ Строка может содержать любые символы.
+
+Примеры:
+xo("ooxx") → true
+xo("xooxx") → false
+xo("ooxXm") → true
+xo("zpzpzpp") → true
+xo("zzoo") → false
+"""
+
+def xo(given_string):
+    given_string = given_string.lower()
+    return given_string.count('x') == given_string.count('o')
+
+print(xo("ooxx"))
+print(xo("xooxx"))
+print(xo("ooxXm"))
+print(xo("zpzpzpp"))
+print(xo("zzoo"))
