610d34fbaeb575ac94f534af2ef9f45e88ac73e2
Viktor <viktor.sbruev.1999@gmail.com>
2025-04-09 19:57:13 +0300

    Modified CW_7_17

--- level_7/CW_7_17.py
+++ level_7/CW_7_17.py
@@ -8,6 +8,8 @@ Given two integers, which can be positive or negative:
 + Найти сумму всех целых чисел между ними (включительно) и вернуть этот результат;
 + Если числа равны, вернуть любое из них;
 + Числа могут быть переданы в любом порядке.
+
+https://www.codewars.com/kata/55f2b110f61eb01779000053
 """
 
 def get_sum(given_number_1, given_number_2):
