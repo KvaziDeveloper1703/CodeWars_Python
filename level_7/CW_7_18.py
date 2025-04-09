1af20512929656d13d082dab049bbab4bef8fa18
Viktor <viktor.sbruev.1999@gmail.com>
2025-04-09 19:56:05 +0300

    Modified CW_7_18

--- level_7/CW_7_18.py
+++ level_7/CW_7_18.py
@@ -1,26 +1,28 @@
 """
-Make a program that filters a list of strings and returns a list containing only your friends' names.
-+ If a name has exactly 4 letters, you can be sure it's a friend!
-+ If the length is not 4, you can be sure they're not.
-+ Keep the original order of names in the output.
-+ Input strings will only contain letters (no digits or symbols).
+Write a function that filters a list of strings and returns a list containing only your friends' names.
++ If a name has exactly 4 letters, you can be sure it's a friend;
++ If the length is not 4, you can be sure they're not;
++ Keep the original order of names in the output;
++ Input strings will only contain letters.
 
-Examples:
-["Ryan", "Kieran", "Jason", "Yous"] → ["Ryan", "Yous"]
-["Peter", "Stephen", "Joe"] → []
+Example:
+Input: ["Ryan", "Kieran", "Jason", "Yous"]
+Output: ["Ryan", "Yous"]
 
-Создайте программу, которая фильтрует список строк и возвращает список, содержащий только имена ваших друзей.
-+ Если имя содержит ровно 4 буквы, это ваш друг!
-+ Если длина имени не равна 4, то этот человек не ваш друг.
-+ Порядок имён в выходном списке должен сохраниться.
-+ Входные строки содержат только буквы (без цифр и символов).
+Напишите функцию, которая фильтрует список строк и возвращает список, содержащий только имена ваших друзей.
++ Если имя содержит ровно 4 буквы, это ваш друг;
++ Если длина имени не равна 4, то этот человек не ваш друг;
++ Порядок имён в выходном списке должен сохраниться;
++ Входные строки содержат только буквы.
 
 Примеры:
-["Ryan", "Kieran", "Jason", "Yous"] → ["Ryan", "Yous"]
-["Peter", "Stephen", "Joe"] → []
+Ввод: ["Ryan", "Kieran", "Jason", "Yous"]
+Вывод: ["Ryan", "Yous"]
+
+https://www.codewars.com/kata/55b42574ff091733d900002f
 """
 
-def friend(given_names):
+def filter_friend(given_names):
 
     filtered_names = []
 
@@ -31,5 +33,5 @@ def friend(given_names):
     return filtered_names
 
 names = ["Ryan", "Kieran", "Jason", "Yous"]
-answer = friend(names)
+answer = filter_friend(names)
 print(answer)
