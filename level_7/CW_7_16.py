89bb28246ee1a3b91c79315e67fa1ea856f54183
Viktor <viktor.sbruev.1999@gmail.com>
2025-04-09 19:33:31 +0300

    Modified CW_7_16

--- level_7/CW_7_16.py
+++ level_7/CW_7_16.py
@@ -1,11 +1,13 @@
 """
-Create a function that returns the sum of the two lowest positive numbers given an array.
+Write a function that returns the sum of the two lowest positive numbers given an array.
 
-Создайте функцию, которая возвращает сумму двух наименьших положительных чисел из переданного массива.
+Напишите функцию, которая возвращает сумму двух наименьших положительных чисел из переданного массива.
+
+https://www.codewars.com/kata/558fc85d8fd1938afb000014
 """
 
-def sum_of_two_smallest_numbers(numbers):
-    sorted_numbers = sorted(numbers)
+def sum_of_two_smallest_numbers(given_numbers):
+    sorted_numbers = sorted(given_numbers)
     return sorted_numbers[0] + sorted_numbers[1]
 
 array = [19, 5, 42, 2, 77]
