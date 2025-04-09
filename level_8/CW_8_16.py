4f0d98dc58781cb457f18c402e16a9d51882356b
Viktor <viktor.sbruev.1999@gmail.com>
2025-04-09 16:12:07 +0300

    Modified CW_8_16

--- level_8/CW_8_16.py
+++ level_8/CW_8_16.py
@@ -2,10 +2,12 @@
 Write a function that converts a two-word name into initials, outputting two capital letters separated by a dot.
 
 Напишите функцию, которая преобразует имя из двух слов в инициалы, выводя две заглавные буквы, разделённые точкой.
+
+https://www.codewars.com/kata/57eadb7ecd143f4c9c0000a3
 """
 
-def abbreviation(name):
-    words = name.split()
+def make_abbreviation(given_name):
+    words = given_name.split()
 
     first_initial = words[0][0]
     last_initial = words[1][0]
@@ -17,6 +19,6 @@ def abbreviation(name):
 
     return initials
 
-name = "patrick feeney"
-answer = abbreviation(name)
+name = "Viktor Sbruev"
+answer = make_abbreviation(name)
 print(answer)
