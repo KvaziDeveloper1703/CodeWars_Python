8f36be3da5867377cf8c80b5fea6116555f3046d
Viktor <viktor.sbruev.1999@gmail.com>
2025-04-09 15:49:10 +0300

    Modified CW_8_13

--- level_8/CW_8_13.py
+++ level_8/CW_8_13.py
@@ -1,17 +1,9 @@
 """
 Consider an array of sheep where some sheep may be missing. You need to write a function that counts the number of sheep present in the array (true means the sheep is present).
 
-Example:
-[True,  True,  True,  False, True,  True,  True,  True, True,  False, True,  False, True,  False, False, True, True,  True,  True,  True, False, False, True,  True]
-
-Answer: 17.
-
 Дан массив овец, где некоторые из них могут отсутствовать. Необходимо написать функцию, которая подсчитает количество присутствующих овец (true означает, что овца на месте).
 
-Пример:
-[True,  True,  True,  False, True,  True,  True,  True, True,  False, True,  False, True,  False, False, True, True,  True,  True,  True, False, False, True,  True]
-
-Ответ: 17.
+https://www.codewars.com/kata/54edbc7200b811e956000556
 """
 
 def count_sheeps(sheeps):
