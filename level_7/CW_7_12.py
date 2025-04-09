d7f7bebb3f292c3c4b045317194f3d00050cb28e
Viktor <viktor.sbruev.1999@gmail.com>
2025-04-09 19:18:41 +0300

    Modified CW_7_12

--- level_7/CW_7_12.py
+++ level_7/CW_7_12.py
@@ -3,21 +3,17 @@ Jaden Smith, the son of Will Smith, is the star of films such as The Karate Kid 
 Your task is to convert strings to how they would be written by Jaden Smith. The strings are actual quotes from Jaden Smith, but they are not capitalized correctly.
 
 Example:
-Not Jaden-Cased:
-"How can mirrors be real if our eyes aren't real"
-
-Jaden-Cased:
-"How Can Mirrors Be Real If Our Eyes Aren't Real"
+Input: "How can mirrors be real if our eyes aren't real"
+Output: "How Can Mirrors Be Real If Our Eyes Aren't Real"
 
 Джейден Смит, сын Уилла Смита, известен своими ролями в фильмах «Каратэ-пацан» (2010) и «После нашей эры» (2013). Он также прославился своими философскими мыслями, которые публикует в Twitter. Когда Джейден пишет в Twitter, он почти всегда делает каждое слово с заглавной буквы.
 Ваша задача — преобразовать строку так, как это сделал бы Джейден Смит. Строки представляют собой его реальные цитаты, но в них отсутствует правильная капитализация.
 
 Пример:
-До преобразования:
-"How can mirrors be real if our eyes aren't real"
+Ввод: "How can mirrors be real if our eyes aren't real"
+Вывод: "How Can Mirrors Be Real If Our Eyes Aren't Real"
 
-После преобразования:
-"How Can Mirrors Be Real If Our Eyes Aren't Real"
+https://www.codewars.com/kata/5390bac347d09b7da40006f6
 """
 
 def to_jaden_case(given_string):
