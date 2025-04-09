5e2eca2a0cf895490d819777e511c46e8e18ec14
Viktor <viktor.sbruev.1999@gmail.com>
2025-04-09 20:11:00 +0300

    Modified CW_7_19

--- level_7/CW_7_19.py
+++ level_7/CW_7_19.py
@@ -1,34 +1,34 @@
 """
-Take two strings string_1 and string_2, containing only letters from 'a' to 'z'.
-+ Return a new sorted string (in alphabetical ascending order), containing distinct letters taken from string_1 or string_2.
+Take two strings containing only letters from 'a' to 'z'.
++ Return a new sorted string, containing distinct letters taken from string_1 or string_2;
 + Each letter should appear only once.
 
-Examples:
-string_1 = "xyaabbbccccdefww"
-string_2 = "xxxxyyyyabklmopq"
-process(string_1, string_2) → "abcdefklmopqwxy"
+Example:
+Input: string_1 = "xyaabbbccccdefww", string_2 = "xxxxyyyyabklmopq"
+Output: "abcdefklmopqwxy"
 
-Даны две строки string_1 и string_2, содержащие только буквы от 'a' до 'z'.
-+ Верните новую отсортированную строку (в алфавитном порядке, по возрастанию), содержащую уникальные буквы, которые встречаются в string_1 или string_2.
+Даны две строки содержащие только буквы от 'a' до 'z'.
++ Верните новую отсортированную строку, содержащую уникальные буквы, которые встречаются в string_1 или string_2;
 + Каждая буква должна встречаться только один раз.
 
-Примеры:
-string_1 = "xyaabbbccccdefww"
-string_2 = "xxxxyyyyabklmopq"
-process(string_1, string_2) → "abcdefklmopqwxy"
+Пример:
+Ввод: string_1 = "xyaabbbccccdefww", string_2 = "xxxxyyyyabklmopq"
+Вывод: "abcdefklmopqwxy"
+
+https://www.codewars.com/kata/5656b6906de340bd1b0000ac
 """
 
-def process(string_1, string_2):
-    combined = string_1 + string_2
+def process_strings(given_string_1, given_string_2):
+    combined = given_string_1 + given_string_2
     unique_characters = []
     
     for character in combined:
         if character not in unique_characters:
             unique_characters.append(character)
     
-    return "".join(sorted(unique_chars))
+    return "".join(sorted(unique_characters))
 
 first_string = "xyaabbbccccdefww"
 second_string ="xxxxyyyyabklmopq"
-answer = process(first_string, second_string)
+answer = process_strings(first_string, second_string)
 print(answer)
