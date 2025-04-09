1f43a5f870ac1c6bde689cac2026a66c4c135800
Viktor <viktor.sbruev.1999@gmail.com>
2025-04-09 20:13:52 +0300

    Modified CW_7_20

--- level_7/CW_7_20.py
+++ level_7/CW_7_20.py
@@ -2,6 +2,8 @@
 Complete the solution so that it returns true if the first string ends with the second string.
 
 Напишите функцию, которая возвращает true, если первая строка заканчивается на вторую строку.
+
+https://www.codewars.com/kata/51f2d1cafc9c0f745c00037d
 """
 
 def solution(given_string, given_ending):
