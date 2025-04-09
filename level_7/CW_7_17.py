87c0d95af1fc108cee9751dad7e326a716c37f2f
Viktor <viktor.sbruev.1999@gmail.com>
2025-04-09 19:35:56 +0300

    Modified CW_7_17

--- level_7/CW_7_17.py
+++ level_7/CW_7_17.py
@@ -1,18 +1,18 @@
 """
-Given two integers a and b, which can be positive or negative:
-+ Find the sum of all integers between them (inclusive) and return it.
-+ If the two numbers are equal, return a or b.
-+ The numbers are not ordered! (a can be greater than b).
+Given two integers, which can be positive or negative:
++ Find the sum of all integers between them (inclusive) and return it;
++ If the two numbers are equal, return either of them;
++ The numbers are not ordered.
 
-Даны два целых числа a и b, которые могут быть положительными или отрицательными.
-+ Найти сумму всех целых чисел между ними (включительно) и вернуть этот результат.
-+ Если числа равны, вернуть a или b (так как сумма будет равна самому числу).
-+ Числа могут быть переданы в любом порядке (то есть a может быть больше b).
+Даны два целых числа, которые могут быть положительными или отрицательными.
++ Найти сумму всех целых чисел между ними (включительно) и вернуть этот результат;
++ Если числа равны, вернуть любое из них;
++ Числа могут быть переданы в любом порядке.
 """
 
-def get_sum(number_1, number_2):
-    lower_bound = min(number_1, number_2)
-    upper_bound = max(number_1, number_2)
+def get_sum(given_number_1, given_number_2):
+    lower_bound = min(given_number_1, given_number_2)
+    upper_bound = max(given_number_1, given_number_2)
 
     numbers = list(range(lower_bound, upper_bound + 1))
 
