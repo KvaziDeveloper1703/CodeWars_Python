f42ae53745c9dc8a0f32cfdf5ed008b8da0378d9
Viktor <viktor.sbruev.1999@gmail.com>
2025-04-09 16:16:27 +0300

    Modified CW_8_19

--- level_8/CW_8_19.py
+++ level_8/CW_8_19.py
@@ -2,12 +2,16 @@
 Given a random non-negative number, return the digits of this number in an array in reverse order.
 
 Дано случайное неотрицательное число. Нужно вернуть массив его цифр в обратном порядке.
+
+https://www.codewars.com/kata/5583090cbe83f4fd8c000051
 """
 
 def digitize(given_number):
     number_as_a_string = str(given_number)
     reversed_string = number_as_a_string[::-1]
-    reversed_digits = [int(digit) for digit in reversed_string]
+    reversed_digits = []
+    for digit in reversed_string:
+        reversed_digits.append(int(digit))
 
     return reversed_digits
 
